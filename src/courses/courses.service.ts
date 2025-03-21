d2ee9510747881c2876d0acf3f276a4190bc9371
Jimmy <jimmy.figueroa@gibobs.com>
2025-03-21 23:19:51 +0100

    feat: first commit

---
+++ src/courses/courses.service.ts
@@ -0,0 +1,41 @@
+import { Injectable } from '@nestjs/common';
+import { CreateCourseDto } from './dto/create-course.dto';
+import { UpdateCourseDto } from './dto/update-course.dto';
+import { Course } from './entities/course.entity';
+
+import { v4 as uuidv4} from 'uuid';
+
+@Injectable()
+export class CoursesService {
+  private courses: Course[];
+  constructor(){
+    this.courses = [];
+  }
+
+  create(createCourseDto: CreateCourseDto) {
+    const course: Course = {
+      id: uuidv4(),
+      name: createCourseDto.name,
+      createdAt: new Date(),
+      ... new Course()
+    }
+    this.courses.push( course );
+    return course;
+  }
+
+  findAll() {
+    return this.courses;
+  }
+
+  findOne(id: string) {
+    return this.courses.find( course => course.id === id );
+  }
+
+  update(id: string, updateCourseDto: UpdateCourseDto) {
+    return `This action updates a #${id} course`;
+  }
+
+  remove(id: string) {
+    return `This action removes a #${id} course`;
+  }
+}
